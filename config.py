cffe4059eda036545dc8af32810164a8bef653fc
Akash Jujare <akash.cdac.mum.aug25@gmail.com>
2025-10-22 09:24:42 +0530

    Add files via upload
    LabourX files added.

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+MYSQL_HOST = "localhost"
+MYSQL_USER = "root"          # replace with your MySQL username
+MYSQL_PASSWORD = "India@11"  # replace with your MySQL password
+MYSQL_DB = "labour_db"
